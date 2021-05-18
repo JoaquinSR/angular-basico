fec4bc29b4a6cb36033378f1998800784f13d562
Joaquin Sanchez <JoaquinSR@users.noreply.github.com>
2021-05-18 20:01:37 +0200

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorModule } from './contador/contador.modules';
+//Tarea crear modulo ContadorModule declara, exporta
+
+import { HeroesModule } from './heroes/heroes.modules';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
